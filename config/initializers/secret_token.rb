1e22c1fc32931949a49884f36e7f352c410af4a0
Riccardo Mastellone (Mac) <riccardo.mastellone@gmail.com>
2013-06-18 10:50:55 +0200

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Belvederedesign::Application.config.secret_token = '45809fef05c48e351b6d4673db9491e6532d59aa2f5cc5bb71a3e791650b3675ee9c761adf9d36584390341ad04dd32100b041bc326ff01dd3a25e6c3ebddebd'
