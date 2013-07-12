e5021c891f84aeaa3273603ed2cb112bd2f3e11b
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2013-07-12 13:29:20 +0200

    con la grafica direi che non ci possiamo lamentare

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,9 @@ module Belvederedesign
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.5'
   end
 end
