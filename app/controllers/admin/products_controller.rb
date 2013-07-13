a2f5acc3d9942317d63448563147ad5ed47605bc
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2013-07-13 11:12:31 +0200

    modifica prodotto ok

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -12,10 +12,22 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    redirect_to '/admin/products/'
+  end
   def new
   end
   def edit
     @product = Product.find(params[:id])
+  end
+  def update
+    p          params[:product]
+    @product= Product.find(params[:id])
+    @product.name =  params[:product]["name"]
+    @product.description =  params[:product]["description"]
+    @product.designer_id =  params["designer_id"]
+    @product.save
+    redirect_to '/admin/products/' + @product.id.to_s
   end
   def destroy
     @product = Product.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :designer_id
   has_one :designer
   has_many :product_images
 end
