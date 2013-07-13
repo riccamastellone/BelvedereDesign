b7ab5044b57ca73e74c39d347bddd1c5aed68da6
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2013-07-13 17:51:32 +0200

    modello categoria

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713080632) do
+ActiveRecord::Schema.define(:version => 20130713155049) do
+
+  create_table "categoria", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "designers", :force => true do |t|
     t.string   "name"
