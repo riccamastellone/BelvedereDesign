638d21b261fde3260dd4ea824b2708e2d0da4b4e
motric <motric@hotmail.it>
2013-07-14 00:09:33 +0200

    primo seed fatto a cazzo
    Domani cambio da creazione singola a ciclo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Designer.delete_all
+Designer.create(name: 'Chris Paul', description: 'Paul frequentò la West Forsyth High School, dove venne nominato Mr. Basketball del Nord Carolina dal Charlotte Observer, il giornale locale. Paul tenne una media di 30,8 punti, 8 assist, 5 rimbalzi e 6 rubate a partita, arrivando alle finali regionali. In una partita segnò 61 punti, sbagliando apposta il libero supplementare (che doveva tirare per aver fatto canestro con fallo) in onore di suo nonno, morto pochi giorni prima alla età di 61 anni.', image_url: 'hi-res-6916468_crop_exact.jpg')
+Designer.create(name: 'Renatone Zero', description: 'Il triangolo noooo', image_url: 'renato.jpg')
 
 categorie = [
     ["Armadi"],
